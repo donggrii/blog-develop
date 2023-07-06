b93717c978697cbaccb4cf1b43a78b6cae17a829
donggrii <ld4230@gmail.com>
2023-07-06 22:25:45 +0900

    test: [READ-ALL] Add dummy data for test

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article (title, content) VALUES ('제목 11', '내용 11')
+INSERT INTO article (title, content) VALUES ('제목 22', '내용 22')
+INSERT INTO article (title, content) VALUES ('제목 33', '내용 33')
