93938edb82fa6e38bfee4d0d720d49a2fc4d2f6c
donggrii <ld4230@gmail.com>
2023-07-09 21:39:33 +0900

    feat: [READ-ALL] Add GetMapping controller method to return articleList

---
+++ src/main/java/com/donggrii/springbootdeveloper/controller/ArticleViewController.java
@@ -0,0 +1,29 @@
+package com.donggrii.springbootdeveloper.controller;
+
+import com.donggrii.springbootdeveloper.dto.ArticleListViewResponseDto;
+import com.donggrii.springbootdeveloper.service.ArticleService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller // 컨트롤러라는 것을 명시적으로 표시 (return 값과 동일한 이름의 html 파일을 찾도록 함)
+public class ArticleViewController {
+
+    private final ArticleService articleService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) { // 뷰로 데이터를 넘겨주는 Model 객체
+        // 블로그 글 전체 리스트를 담은 뷰를 반환
+        List<ArticleListViewResponseDto> articles = articleService.findAll()
+                                                                  .stream()
+                                                                  .map(
+                                                                      ArticleListViewResponseDto::new)
+                                                                  .toList();
+        model.addAttribute("articles", articles); // 블로그 글 리스트를 Model 객체에 저장
+
+        return "articleList"; // articleList.html 라는 뷰 조회
+    }
+}
