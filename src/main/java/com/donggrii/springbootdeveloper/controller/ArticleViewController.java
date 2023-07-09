69e8811ab173566bc6477221eadc3cdf45b1aa50
donggrii <ld4230@gmail.com>
2023-07-10 02:55:07 +0900

    feat: [UPDATE, CREATE] Add GetMapping controller method to return newArticle

--- src/main/java/com/donggrii/springbootdeveloper/controller/ArticleViewController.java
+++ src/main/java/com/donggrii/springbootdeveloper/controller/ArticleViewController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller // 컨트롤러라는 것을 명시적으로 표시 (return 값과 동일한 이름의 html 파일을 찾도록 함)
@@ -36,5 +37,18 @@ public class ArticleViewController {
         model.addAttribute("article", new ArticleViewResponseDto(article));
 
         return "article"; // article.html 라는 뷰 조회
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        // @RequestParam(required = false)으로 id 키를 가진 쿼리 파라미터의 값을 id 변수에 매핑 (id는 Optional)
+        if (id == null) { // id가 없으면 생성
+            model.addAttribute("article", new ArticleViewResponseDto());
+        } else { // id가 있으면 수정
+            Article article = articleService.findById(id);
+            model.addAttribute("article", new ArticleViewResponseDto(article));
+        }
+
+        return "newArticle";
     }
 }
