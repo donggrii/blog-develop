fbae8b65757dafd19013f3de55f989f17f632847
donggrii <ld4230@gmail.com>
2023-07-11 00:31:09 +0900

    feat: [UPDATE] Add update API

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -1,15 +1,42 @@
 // 삭제 기능
-const deleteButton = document.getElementById('delete-btn');
+const deleteButton = document.getElementById("delete-btn");
 
 if (deleteButton) {
-  deleteButton.addEventListener('click', event => {
-    let id = document.getElementById('article-id').value;
+  deleteButton.addEventListener("click", event => {
+    let id = document.getElementById("article-id").value;
     fetch(`/api/articles/${id}`, {
-      method: 'DELETE'
+      method: "DELETE"
     })
     .then(() => {
-      alert('삭제가 완료되었습니다.');
-      location.replace('/articles');
+      alert("삭제가 완료되었습니다.");
+      location.replace("/articles");
+    });
+  });
+}
+
+// 수정 기능
+// id가 modify-btn인 엘리먼트 조회
+const modifyButton = document.getElementById("modify-btn");
+
+if (modifyButton) {
+  // 클릭 이벤트가 감지되면 수정 API 요청
+  modifyButton.addEventListener("click", event => {
+    let params = new URLSearchParams(location.search); // URL에서 쿼리 파라미터 값 찾기
+    let id = params.get("id");
+
+    fetch(`/api/articles/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: document.getElementById("title").value,
+        content: document.getElementById("content").value
+      })
+    })
+    .then(() => {
+      alert("수정이 완료되었습니다.");
+      location.replace(`/articles/${id}`); // backtick : ${}로 감싸져 있는 것을 변수로 인식하게 함
     });
   });
 }
